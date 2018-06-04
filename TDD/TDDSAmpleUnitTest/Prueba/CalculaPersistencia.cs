ab10505a219d9e7beb29d5e638ab83b1dce47405
Jorge Escobar <jorge.escobar@truextend.com>
2018-06-04 09:06:13 -0400

    Código más unit Test

---
+++ TDD/TDDSAmpleUnitTest/Prueba/CalculaPersistencia.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    public class CalculaPersistencia
+    {
+        public int Calcula(long numero)
+        {
+            /// contar las veces que el resultado de la multiplicación de los dígitos devuelve un número de más de un dígito
+            int suma = 0;
+            var resultado = 1;
+            var resultadoCadena = numero.ToString();
+            int longitudCadena = resultadoCadena.Length;
+            while (resultadoCadena.Length > 1)
+            {
+                for (int pos = 0; pos <= resultadoCadena.Length-1; pos++ )
+                {
+                    resultado *= Convert.ToInt32(resultadoCadena[pos].ToString());
+                }
+                suma++;
+                resultadoCadena = resultado.ToString();
+                resultado = 1;
+            }
+            
+            return suma;
+        }
+
+
+
+
+    }
+}
+
---
+++ TDD/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ObtenerPersistencia_UnDigito_Cero()
+        {
+            long numeroPrueba = 2;
+            int esperado = 0;
+            var persistencia = new Prueba.CalculaPersistencia();
+            var resultado = persistencia.Calcula(numeroPrueba);
+
+            Assert.AreEqual(resultado, esperado);
+        }
+
+        [TestMethod]
+        public void ObtenerPersistencia_DosDigitos_Uno()
+        {
+            long numeroPrueba = 23;
+            int esperado = 1;
+            var persistencia = new Prueba.CalculaPersistencia();
+            var resultado = persistencia.Calcula(numeroPrueba);
+
+            Assert.AreEqual(resultado, esperado);
+        }
+
+        [TestMethod]
+        public void ObtenerPersistencia_TresDigitos_Uno()
+        {
+            long numeroPrueba = 123;
+            int esperado = 2;
+            var persistencia = new Prueba.CalculaPersistencia();
+            var resultado = persistencia.Calcula(numeroPrueba);
+
+            Assert.AreEqual(resultado, esperado);
+        }
+
+        [TestMethod]
+        public void ObtenerPersistencia_TresDigitos_Dos()
+        {
+            long numeroPrueba = 1234;
+            int esperado = 2;
+            var persistencia = new Prueba.CalculaPersistencia();
+            var resultado = persistencia.Calcula(numeroPrueba);
+
+            Assert.AreEqual(resultado, esperado);
+        }
+
+        [TestMethod]
+        public void ObtenerPersistencia_CincoDigitos12345_Dos()
+        {
+            long numeroPrueba = 12345;
+            int esperado = 2;
+            var persistencia = new Prueba.CalculaPersistencia();
+            var resultado = persistencia.Calcula(numeroPrueba);
+
+            Assert.AreEqual(resultado, esperado, "12345 = 120 que equivale a 2 veces");
+        }
+
+        [TestMethod]
+        public void ObtenerPersistencia_CincoDigitos99999_Dos()
+        {
+            long numeroPrueba = 99999;
+            int esperado = 2;
+            var persistencia = new Prueba.CalculaPersistencia();
+            var resultado = persistencia.Calcula(numeroPrueba);
+
+            Assert.AreEqual(resultado, esperado, numeroPrueba +" = que equivale a 2 veces");
+        }
+
+        [TestMethod]
+        public void ObtenerPersistencia_CincoDigitos77777_Dos()
+        {
+            long numeroPrueba = 77777;
+            int esperado = 2;
+            var persistencia = new Prueba.CalculaPersistencia();
+            var resultado = persistencia.Calcula(numeroPrueba);
+
+            Assert.AreEqual(resultado, esperado, numeroPrueba + " = que equivale a 2 veces");
+        }
+    }
+}
